70f0027fc2d50e716d481f604125e7ec76dd33e9
kai2june <104703027@nccu.edu.tw>
2018-06-07 00:06:34 +0800

    Finish buyer paid and unlock whole procedure. Mailer merchandiseArriveLocker still need to be dealt with.

--- src/routes/payRoutes.js
+++ src/routes/payRoutes.js
@@ -12,26 +12,35 @@ const router = function(nav){
             next();
         }
     });
-    payRouter.get('/', (req,res) => {
-        // (async function moneyPaid(){
-        //     const url = 'mongodb://localhost:27017';
-        //     const dbName = 'libraryApp';
+    payRouter
+        .get('/', (req,res) => {
+            res.render('unlockLocker');
+        })
+        .post('/', (req,res) => {
+            (async function moneyPaid(){
+                const url = 'mongodb://localhost:27017';
+                const dbName = 'libraryApp';
 
-        //     try{
-        //         const client = await MongoClient.connect(url);
-        //         const db = client.db(dbName);
-        //         const coll = db.collection('orders');
-        //         const results = await coll.find({
+                try{
+                    const client = await MongoClient.connect(url);
+                    const db = client.db(dbName);
+                    const coll = db.collection('orders');
+                    const results_findSingleOrder = await coll.findOne({
+                        buyerName: req.user.username, 
+                        locker: req.body.locker
+                    });
+                    if(!results_findSingleOrder)
+                        res.send(`You have not placed an order.\n or\n You chose wrong locker, your merchandise was placed in other locker, go to buyRecord to check.`);
+                    else if (results_findSingleOrder.moneyPaid == false)
+                        res.send(`You have not paid ether.\nAfter paying money, press f5 to refresh then click 解鎖!!!`);
+                    else
+                        res.render('../../server/index');
+                }catch(err){
+                    console.log(err);
+                }
+            }());
+        });
 
-        //         });
-        //     }catch(err){
-        //         console.log(err);
-        //     }
-            
-        // }());
-
-        res.render('../../server/index');
-    });
 
     return payRouter;
 };
